bf64bb1782d77001485c60daa07dbc2701e3965c
Ege Çetin <ege.cetin@pavotek.com.tr>
2025-07-31 10:26:48 +0300

    pre-commit run

--- Examples/dump-to-file-example.cpp
+++ Examples/dump-to-file-example.cpp
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 /**
- * This example is similar to "basic-example", the only difference is that it shows how to dump data to a file. It demonstrates the following flow:
+ * This example is similar to "basic-example", the only difference is that it shows how to dump data to a file. It
+ * demonstrates the following flow:
  * - Start collecting memory allocation data and dump data to a file
  * - Allocate some memory (2 objects and 1 array)
- * - Run a mem leak test in verbose mode and dump data to a file. This will output all places in the code where memory was allocated and not yet freed.
- *   It will also summarize the total number of leaked objects as well as the amount of memory leaked
+ * - Run a mem leak test in verbose mode and dump data to a file. This will output all places in the code where memory
+ * was allocated and not yet freed. It will also summarize the total number of leaked objects as well as the amount of
+ * memory leaked
  * - Free some memory. This will also be dumped to the file
  * - Stop collecting memory allocation data and free remaining leaked memory. This will also be logged into the file
  *
@@ -36,44 +38,48 @@
  *
  */
 
-
-class MyClass {
-    int x;
+class MyClass
+{
+	int x;
 };
 
-int main( int argc, const char* argv[]) {
+int main(int argc, const char* argv[])
+{
 
-    // start collecting mem allocations info and dump info to "mem_allocations.log"
-    MemPlumber::start(true, "mem_allocations.log");
+	// start collecting mem allocations info and dump info to "mem_allocations.log"
+	MemPlumber::start(true, "mem_allocations.log");
 
-    // init 2 objects
-    int* num = new int(100);
-    MyClass* myClass = new MyClass();
+	// init 2 objects
+	int* num = new int(100);
+	MyClass* myClass = new MyClass();
 
-    // init 1 array of 10 objects
-    MyClass* myClassArr = new MyClass[10];
+	// init 1 array of 10 objects
+	MyClass* myClassArr = new MyClass[10];
 
-    // run memory leak test in verbose mode and dump info to "mem_leak_test.log"
-    size_t memLeakCount;
-    uint64_t memLeakSize;
-    MemPlumber::memLeakCheck(memLeakCount, memLeakSize, true, "mem_leak_test.log");
+	// run memory leak test in verbose mode and dump info to "mem_leak_test.log"
+	size_t memLeakCount;
+	uint64_t memLeakSize;
+	MemPlumber::memLeakCheck(memLeakCount, memLeakSize, true, "mem_leak_test.log");
 
-    // print memory leak results
-    printf("1st run. Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCount, (int)memLeakSize);
+	// print memory leak results
+	printf("1st run. Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCount,
+	       (int)memLeakSize);
 
-    // free some memory (this will also be written to "mem_allocations.log")
-    delete num;
-    delete [] myClassArr;
+	// free some memory (this will also be written to "mem_allocations.log")
+	delete num;
+	delete[] myClassArr;
 
-    // run memory leak test again in verbose mode and dump info to the same file "mem_leak_test.log" but in append mode
-    // this time we're expecting to find only 1 leaked object (myClass) since the other were already freed
-    MemPlumber::memLeakCheck(memLeakCount, memLeakSize, true, "mem_leak_test.log", true);
+	// run memory leak test again in verbose mode and dump info to the same file "mem_leak_test.log" but in append mode
+	// this time we're expecting to find only 1 leaked object (myClass) since the other were already freed
+	MemPlumber::memLeakCheck(memLeakCount, memLeakSize, true, "mem_leak_test.log", true);
 
-    // print memory leak results
-    printf("\n2nd run. Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCount, (int)memLeakSize);
+	// print memory leak results
+	printf("\n2nd run. Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCount,
+	       (int)memLeakSize);
 
-    // stop collecting mem allocations info and clean the reminaing mem leaks. This will also be dumped to "mem_allocations.log"
-    MemPlumber::stopAndFreeAllMemory();
+	// stop collecting mem allocations info and clean the reminaing mem leaks. This will also be dumped to
+	// "mem_allocations.log"
+	MemPlumber::stopAndFreeAllMemory();
 
-    return 0;
+	return 0;
 }
